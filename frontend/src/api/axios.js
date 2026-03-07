369a11ccd3c02a913eec064f340a27795dbb8cf5
Youssef <youssefrua11@gmail.com>
2026-03-07 17:05:02 +0200

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8081"
-    : "https://printova.onrender.com";
+    : "https://printovaa.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
